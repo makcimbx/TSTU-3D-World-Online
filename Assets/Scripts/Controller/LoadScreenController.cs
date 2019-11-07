8b8995831a38b60d28b8b055b91c4ff563b9d7da
keliz <keliz@ya.ru>
2019-11-07 13:15:54 +0300

    Добавил jopablin и loadscreen

---
+++ Assets/Scripts/Controller/LoadScreenController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace TSTU.Controller
+{
+    public class LoadScreenController : MonoBehaviour
+    {
+        [SerializeField]
+        private Button SignIn;
+        [SerializeField]
+        private InputField Login;
+        [SerializeField]
+        private InputField Password;
+
+
+        private void Start()
+        {
+            SignIn.onClick.AddListener(OnSignIn);
+        }
+
+
+        public void OnSignIn()
+        {
+            Debug.Log($"Ввод параши { Login.text} - {Password.text}");
+            SceneManager.LoadScene(1,LoadSceneMode.Single);
+        }
+    }
+
+}
