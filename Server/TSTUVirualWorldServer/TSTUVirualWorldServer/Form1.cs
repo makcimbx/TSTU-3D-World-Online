e07015d6d5debc588fd32bb673078772b2e45bd3
Makcim Ivanov <makcimbx@gmail.com>
2019-11-07 11:28:38 +0300

    Game Server Initialize UDP

---
+++ Server/TSTUVirualWorldServer/TSTUVirualWorldServer/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TSTUVirualWorldServer
+{
+    public partial class Form1 : Form
+    {
+        private static GameServer gameServer;
+        private static Thread thread;
+
+        private int localPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            localPort = int.Parse(textBox1.Text);
+
+            gameServer = new GameServer(this, localPort);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            thread = new Thread(gameServer.StartListening);
+            thread.Start();
+            richTextBox1.Text += $"Начинаем прослушивать порт {localPort}\n";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            gameServer.StopListening();
+            if (thread != null) thread.Abort();
+            richTextBox1.Text += $"Перестаем прослушивать порт {localPort}\n";
+        }
+    }
+}
---
+++ Server/TSTUVirualWorldServer/TSTUVirualWorldServer/GameServer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TSTUVirualWorldServer
+{
+    public class GameServer
+    {
+        private int localPort;
+        private Form1 form;
+
+        private UdpClient receiver;
+
+        public GameServer(Form1 form, int localPort)
+        {
+            this.form = form;
+            this.localPort = localPort;
+        }
+
+        public void StartListening()
+        {
+            StopListening();
+            receiver = new UdpClient(localPort); // UdpClient для получения данных
+            IPEndPoint remoteIp = null; // адрес входящего подключения
+            try
+            {
+                while (true)
+                {
+                    byte[] data = receiver.Receive(ref remoteIp); // получаем данные
+                    string message = Encoding.Unicode.GetString(data);
+                    form.richTextBox1.Invoke(new MethodInvoker(() =>
+                    {
+                        form.richTextBox1.Text += $"Собеседник: {message}\n";
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                receiver.Close();
+            }
+        }
+
+        public void StopListening()
+        {
+            if(receiver != null)
+                receiver.Close();
+        }
+
+        private void SendMessage(string message ,string remoteAddress, int remotePort)
+        {
+            UdpClient sender = new UdpClient(); // создаем UdpClient для отправки сообщений
+            try
+            {
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                sender.Send(data, data.Length, remoteAddress, remotePort); // отправка
+            }
+            catch (Exception ex)
+            {
+                form.richTextBox1.Invoke(new MethodInvoker(() =>
+                {
+                    form.richTextBox1.Text += $"{ex}\n";
+                }));
+            }
+            finally
+            {
+                sender.Close();
+            }
+        }
+    }
+}
